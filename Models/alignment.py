e087470a8a1947bf30933ccb3d6c86040a9e2960
YifDuan <2291236972@qq.com>
2026-09-24 15:05:46 +0800

    Delete Models directory

--- Models/alignment.py
+++
@@ -1,9 +0,0 @@
-import torch.nn.functional as F
-
-
-def c_loss(estimated, target, alpha, beta):
-    """Feature alignment loss: MSE plus cosine distance."""
-    mse = F.mse_loss(estimated, target)
-    cosine = F.cosine_similarity(estimated, target, dim=-1, eps=1e-8)
-    cosine_loss = (1.0 - cosine).mean()
-    return alpha * mse + beta * cosine_loss
